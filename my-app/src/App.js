4ffe75ec51b5075cd877b9faf9492699464223a3
wewlr17 <gabindepaire@gmail.com>
2021-02-22 16:16:26 +0100

    init auth

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,11 @@
+import { Provider, useSelector, useDispatch } from "react-redux";
+import Authentication from "./User/Authentication";
+
 
 function App() {
+  const isAuthenticated = useSelector(
+    (state) => state.app.user !== null
+  );
   return (
     <h1>BONJOUR</h1>
   );
